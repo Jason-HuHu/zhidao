148626356d67eec606661ec43380fcf6c0109e41
huxl <xiaohucomeon@hotmail.com>
2017-11-09 00:05:29 +0800

    用户登录功能部分代码

---
+++ zhidao/src/main/java/com/huxl/zhidao/dao/UserDAO.java
@@ -0,0 +1,36 @@
+package com.huxl.zhidao.dao;
+
+import com.huxl.zhidao.model.User;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author huxingl
+ * @Date 2017/11/6
+ * 用户Dao接口
+ */
+@Repository
+@Mapper
+public interface UserDAO {
+    String TABLE_NAME = " user ";
+    String INSERT_FIELDS = " user_name, password, salt, head_url";
+    String SELECT_FIELDS = " user_id, " + INSERT_FIELDS;
+
+    @Insert({"insert into", TABLE_NAME,"(",INSERT_FIELDS,") values (#{userName},#{password}),#{salt},#{headUrl}" })
+    int addUser(User user);
+
+    @Select({"select", SELECT_FIELDS, "from", TABLE_NAME, "where user_id = #{id}"})
+    User selectById(int id);
+
+    @Select({"select", SELECT_FIELDS, "from", TABLE_NAME, "where user_name = {userName}"})
+    User selectByName(String userName);
+
+    @Update({"update", TABLE_NAME, "set password = #{password} where user_id = #{userId}"})
+    void updatePassword(User user);
+
+    @Update({"update", TABLE_NAME, "set head_url = #{headUrl} where user_id = #{userId}"})
+    void updateHeadUrl(User user);
+
+    @Delete({"delete from", TABLE_NAME, "where user_id = #{id}"})
+    void deleteById(int id);
+}
--- zhidao/src/main/java/com/huxl/zhidao/service/UserService.java
+++ zhidao/src/main/java/com/huxl/zhidao/service/UserService.java
@@ -1,10 +1,13 @@
 package com.huxl.zhidao.service;
 
-import com.huxl.zhidao.dao.UserDao;
+import com.huxl.zhidao.dao.UserDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author huxingl
@@ -15,6 +18,15 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
+
     @Autowired
-    private UserDao userDao;
+    UserDAO userDAO;
+
+    public Map<String,String> login(String username, String password) {
+        Map<String,String> map = new HashMap<>();
+        return map;
+    }
+
+
+
 }

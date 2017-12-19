5fa24a3e54de30cdd5a13b63ebc975f910278ac8
huxl <1259450641@qq.com>
2017-12-19 08:56:02 +0800

    实体

---
+++ zhidao/src/main/java/com/huxl/zhidao/controller/QuestionComntroller.java
@@ -0,0 +1,57 @@
+package com.huxl.zhidao.controller;
+
+import com.huxl.zhidao.model.HostHolder;
+import com.huxl.zhidao.model.Question;
+import com.huxl.zhidao.service.QuestionService;
+import com.huxl.zhidao.util.ZhidaoUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+/**
+ * @author huxl
+ * @createDate 2017/12/16 15:26
+ */
+@Controller
+public class QuestionComntroller {
+    private static final Logger logger = LoggerFactory.getLogger(QuestionComntroller.class);
+
+    @Autowired
+    QuestionService questionService;
+    @Autowired
+    HostHolder hostHolder;
+
+    @RequestMapping(path = {"/question/add"},method = {RequestMethod.POST})
+    @ResponseBody
+    public String addQuestion(@RequestParam("title") String title,
+                              @RequestParam("content") String content) {
+        try {
+            Question question = new Question();
+            question.setTitle(title);
+            question.setContent(content);
+            question.setCreatedDate(new Date());
+            if (hostHolder.getUser() == null) {
+                question.setUserId(ZhidaoUtil.ANOYMOUS_USERID);
+            } else {
+                question.setUserId(hostHolder.getUser().getUserId());
+            }
+
+            if (questionService.addQuestion(question) > 0) {
+                return ZhidaoUtil.getJSONString(0);
+            }
+
+        } catch (Exception e) {
+            logger.error("添加问题失败",e.getMessage());
+        }
+
+        return ZhidaoUtil.getJSONString(1, "添加失败");
+
+    }
+}
